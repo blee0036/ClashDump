58bf341954c256cd315ee59f4b05ffa618864684
chriswilson <jzm@outlook.sk>
2023-02-14 16:57:21 +0800

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ subprojects {
             minSdk = 21
             targetSdk = 31
 
-            versionName = "11.45.14"
-            versionCode = 114514
+            versionName = "11.45.15"
+            versionCode = 114515
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
